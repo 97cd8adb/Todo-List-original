c4de77a6f00f49fc1f1405d22563f5d459508c20
1sh1d4 <isd97cd8adb@icloud.com>
2025-08-02 14:56:17 +0900

    見映えを優先

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-
+const App = () => {
+  return (
+    <>
+    <div className="InputTodoArea">
+      <input type="text" placeholder="Todoを入力" />
+      <input type="button" value="追加"/>
+    </div>
+    <div className="IncompleteTodoArea">
+      <h1>未完了のTodo</h1>
+      <ul>
+        <li>
+          Todo1
+          <input type="button" value="完了" />
+          <input type="button" value="削除" />
+        </li>
+        <li>
+          Todo2
+          <input type="button" value="完了" />
+          <input type="button" value="削除" />
+        </li>
+      </ul>
+    </div>
+    <div className="CompleteTodoArea">
+      <h1>完了のTodo</h1>
+      <ul>
+        <li>
+          Todo1
+          <input type="button" value="削除" />
+        </li>
+        <li>
+          Todo1
+          <input type="button" value="削除" />
+        </li>
+      </ul>
+    </div>
+    </>
+  )
 }
 
 export default App
