19f3398b2fff90008f72be9fd4efe6a7480bc037
1sh1d4 <isd97cd8adb@icloud.com>
2025-08-02 17:52:42 +0900

    入力機能、完了機能実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,60 @@ import { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  //入力フォーム
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([])
+  const [completeTodos, setCompleteTodos] = useState([])
+  const onChangeText = ( e ) => {
+    //eから入力値を取得し、状態にセット
+    setTodoText(e.target.value)
+  }
+  const onClickAdd = () => {
+    //入力が空なら何もしない,新しいTodoをincompleteTodosに追加,入力欄を空に戻す
+    if(todoText === "") return;
+    setIncompleteTodos([...incompleteTodos, todoText]);
+    setTodoText("")
+  }
+  const onClickComplete = (targetIndex) => {
+    const newTodos = incompleteTodos.filter((todo, index) => index !== targetIndex );
+    setIncompleteTodos(newTodos);
+    setCompleteTodos([...completeTodos, incompleteTodos[targetIndex]])
+  }
+
   return (
     <>
     <div className="InputTodoArea">
-      <input type="text" placeholder="Todoを入力" />
-      <input type="button" value="追加"/>
+      <input
+        value={todoText}
+        onChange={onChangeText}
+        type="text"
+        placeholder="Todoを入力"
+      />
+      <input
+        onClick={onClickAdd}
+        type="button"
+        value="追加"
+      />
     </div>
     <div className="IncompleteTodoArea">
-      <h1>未完了のTodo</h1>
+      <p>未完了のTodo</p>
       <ul>
-        <li>
-          Todo1
-          <input type="button" value="完了" />
-          <input type="button" value="削除" />
-        </li>
-        <li>
-          Todo2
-          <input type="button" value="完了" />
-          <input type="button" value="削除" />
-        </li>
+        {incompleteTodos.map((todo, index) => 
+          <li key={index} >
+            {todo}
+            <input onClick={()=> onClickComplete(index)} type="button" value="完了" />
+            <input type="button" value="削除" />
+          </li>
+        )}
       </ul>
     </div>
     <div className="CompleteTodoArea">
-      <h1>完了のTodo</h1>
+      <p>完了のTodo</p>
       <ul>
-        <li>
-          Todo1
-          <input type="button" value="削除" />
-        </li>
-        <li>
-          Todo1
-          <input type="button" value="削除" />
-        </li>
+          <li key={index} >
+            {todo}
+            <input onClick={()=> onClickComplete(index)} type="button" value="完了" />
+          </li>
       </ul>
     </div>
     </>
